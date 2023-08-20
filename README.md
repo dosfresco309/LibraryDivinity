2f47aee4ed18bd20ba8705aba79ec90e51af0805
dosfresco309 <141143195+dosfresco309@users.noreply.github.com>
2023-08-19 23:17:08 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Disney Wall Parallax [Only CSS]
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/designfenix/pen/xxWeEQV](https://codepen.io/designfenix/pen/xxWeEQV).
+
---
+++ script.js
@@ -0,0 +1,11 @@
+/*nope*/
+/*$(document).mousemove(function (event) {
+	let xPos = event.clientX / $(window).width() - 0.5,
+		yPos = event.clientY / $(window).height() - 0.5;
+
+	gsap.to($(".wrapper-images"), 0.6, {
+		rotateY: (xPos * 10) / 2 + "deg",
+		rotateX: (yPos * 10) / -2 + "deg",
+		ease: Power4.easeOut
+	});
+});*/
